b135e2e0bb78d7ed0967422787fb2b063a64fd98
Gabriel Domingos <gdomingoss11@gmail.com>
2021-05-28 19:29:57 -0400

    adicionando novas melhorias

--- src/test/java/com/gabriel/conversortemperatura/AppTest.java
+++ src/test/java/com/gabriel/conversortemperatura/AppTest.java
@@ -10,16 +10,28 @@ import org.junit.jupiter.api.BeforeEach;
 public class AppTest 
 {
     private Conversor conv;
+    private Conversor conv2;
+    private Conversor conv3;
 	
     @BeforeEach
 	public void setup() {
 		conv = new Conversor(28);
+		conv2 = new Conversor(55);
+		conv3 = new Conversor(40);
+		
 	}
     
     @Test
     public void testeConversao() {
         conv.alterar();
-
-        assertEquals(conv.getTemp().getTemperatura(), 82.40);
+        conv2.alterar();
+        conv3.alterar();
+        double resultado1,resultado2,resultado3;
+        resultado1 = conv.getTemp().getTemperatura();
+        resultado2 = conv2.getTemp().getTemperatura();
+        resultado3 = conv3.getTemp().getTemperatura();
+        assertEquals(resultado1, 82.40);
+        assertEquals(resultado2, 131.00);
+        assertEquals(resultado3, 104.00);
     }
 }
