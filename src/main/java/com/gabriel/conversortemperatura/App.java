e5d5e1f8ffe03868c80ac8bab1b6d6a62a8ac2f2
Gabriel Domingos <gdomingoss11@gmail.com>
2021-05-30 20:03:20 -0400

    alteracoes com erros

--- src/main/java/com/gabriel/conversortemperatura/App.java
+++ src/main/java/com/gabriel/conversortemperatura/App.java
@@ -5,11 +5,9 @@ import java.util.Scanner;
 public class App 
 {
     public static void main( String[] args )
-    {	
     	Scanner entrada = new Scanner(System.in);
     	System.out.println("Digite um valor em Celsius para converter para Fahrenheit: ");
     	int valor = entrada.nextInt();
 		Conversor conv = new Conversor(valor);
 		conv.alterar();
-	}
 }
